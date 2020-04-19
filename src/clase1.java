aa9532e229101905154f7b870531d488721bff87
Maeso <tony_maeso@hotmail.com>
2020-04-19 17:12:43 +0200

    Creacion clases

--- src/clase1.java
+++ src/clase1.java
@@ -1,7 +1,21 @@
-
+import java.util.Scanner;
 public class clase1 {
 
 	public static void main (String[]args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int [] num = new int [a];
+		Contar(num, a, sc);
+		
+	}
+	
+	public static void Contar (int [] num, int a, Scanner sc) {
+		 for(int i = 0; i<num.length; i++) {
+			 num[i]=sc.nextInt();
+	            if(num[i]%10 == 3) {
+	                System.out.println(num[i]+ " acaba en 3");
+	            }
+	}
 	}
 }
--- src/clase2.java
+++ src/clase2.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+public class clase {
 
-public class clase2 {
-
-	public static void main(String[] args) {
-
+	public static void main (String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Tamaño array");
+		int a = sc.nextInt();
+		int [] num = new int [a];
+		Media(num, a, sc);
+		
 	}
-
+	
+	public static void Media (int [] num, int a, Scanner sc) {
+		int media=0;
+		int b=0;
+		for(int i = 0; i<num.length; i++) {
+			 System.out.println("Introduce los números");
+			 num[i]=sc.nextInt();
+			 b=b+num[i];
+		 }
+		media=b/num.length;
+		 System.out.println(media);
+	}
 }
