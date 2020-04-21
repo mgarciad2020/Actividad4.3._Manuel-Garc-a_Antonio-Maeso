1dc0a68b2b45293c97539f1db4d6aa0a29db9f8d
Manuel <63641164+mgarciad2020@users.noreply.github.com>
2020-04-21 18:11:04 +0200

    Update clase1.java

--- src/clase1.java
+++ src/clase1.java
@@ -3,19 +3,21 @@ public class clase1 {
 
 	public static void main (String[]args) {
 		
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int [] num = new int [a];
-		Contar(num, a, sc);
+		int [] num = new int [5];
+		System.out.println(Contar(num));
 		
 	}
 	
-	public static void Contar (int [] num, int a, Scanner sc) {
-		 for(int i = 0; i<num.length; i++) {
+		public static int Contar(int num []) {
+		Scanner sc = new Scanner(System.in);
+		int contador = 0;
+		for(int i = 0; i<num.length; i++) {
 			 num[i]=sc.nextInt();
 	            if(num[i]%10 == 3) {
 	                System.out.println(num[i]+ " acaba en 3");
+	                contador++;
 	            }
 	}
+		return contador;
 	}
 }
