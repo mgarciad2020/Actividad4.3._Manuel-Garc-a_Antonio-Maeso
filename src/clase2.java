2fd95b6c7f1045bf5874ef5ce643ec5bf35b383e
Maeso <tony_maeso@hotmail.com>
2020-04-27 18:05:59 +0200

    javadoc clase2

--- src/clase2.java
+++ src/clase2.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
+/**
+ * 
+ * @author Maeso
+ *
+ */
 public class clase2 {
 
+	/**
+	 * 
+	 * @param args
+	 * Llama a la función media, que calcula la media de los números dentro de "num".
+	 */
 	public static void main (String[]args) {
 		
 		int [] num = new int [9];
@@ -8,6 +18,11 @@ public class clase2 {
 		
 	}
 	
+	/**
+	 * Esta función nos permite introducir números y calcular la media de dichos números.
+	 * @param num array en el que introduciremos los números de los que queremos saber la media.
+	 * @return retorna el valor de la variable "media".
+	 */
 	public static int Media (int [] num) {
 		int media=0;
 		int b=0;
